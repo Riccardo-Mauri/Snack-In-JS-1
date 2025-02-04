fde0b115460002c765756412e8a493c32bc1ed0a
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2025-02-04 15:36:14 +0100

    snack eseguito

--- main.js
+++ main.js
@@ -3,5 +3,19 @@ const users = [
     { name: 'Jane', role: 'Manager' },
     { name: 'Jim', role: 'Designer' },
     { name: 'Mark', role: 'Developer' }
-  ];
-  
+];
+
+
+//inizio creando un array vuoto dove memorizzerò poi i nomi degli sviluppatori (Developer)
+
+const devs = [];
+
+//ora eseguo un ciclo for dell'array 'users' per filtrare gli sviluppatori (Developer)
+for (let i = 0; i < users.length; i++) {
+    //quindi SE l'utente ha il ruolo di 'Developer' aggiungo il suo nome all'array di devs
+    if (users[i].role === 'Developer') {
+        devs.push(users[i].name)
+    }
+}
+//stampo quindi il risultato in console 
+console.log('I Developer sono:',devs);
